16640ed9ebe2f7c4224b873cfbf1d3112a9976b7
Richard Hildred <rhildred@gmail.com>
2019-02-25 15:13:23 -0500

    oops cloned wrong repo

--- toaster.sh
+++ toaster.sh
@@ -1,6 +1,6 @@
 [ -d "./UX320/Feb25-database-child-theme" ] && exit
 mkdir -p UX320
 cd ./UX320
-git clone https://github.com/rhildred/UX320-database-child-theme.git Feb25-database-child-theme
+git clone https://github.com/rhildred/wordpress-heroku.git Feb25-database-child-theme
 cd Feb25-database-child-theme/wp-content/plugins
 git clone https://github.com/rhildred/wordpress_database_admin.git
