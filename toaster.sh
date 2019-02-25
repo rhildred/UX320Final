1562d85c92d7a50852aa8271dd22e056461a78c5
Richard Hildred <rhildred@gmail.com>
2019-02-25 12:52:35 -0500

    Create toaster.sh

---
+++ toaster.sh
@@ -0,0 +1,6 @@
+cd ~
+mkdir -p UX320
+cd ~/UX320
+git clone https://github.com/rhildred/UX320-database-child-theme.git Feb25-database-child-theme
+cd Feb25-database-child-theme/wp-content/plugins
+git clone https://github.com/rhildred/wordpress_database_admin.git
