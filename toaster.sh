106f8aa29bd096a2e195a46817c3611df5530625
Rich Hildred <rhildred>
2019-03-11 23:01:31 +0000

    think I am ready for a smoke test

--- toaster.sh
+++ toaster.sh
@@ -5,3 +5,4 @@ git clone https://github.com/rhildred/wordpress-heroku.git Mar11-angular-plugin
 cd Mar11-angular-plugin/wp-content/plugins
 git clone https://github.com/rhildred/wordpress_database_admin.git
 git clone https://github.com/rhildred/carserviceform.git
+git clone https://github.com/rhildred/piday-plugin.git
