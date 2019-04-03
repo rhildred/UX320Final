e8cede4d96fe19799dca066e9d6c4fa244708147
Rich Hildred <rhildred>
2019-04-03 16:51:14 +0000

    initial update to it's own lab

--- toaster.sh
+++ toaster.sh
@@ -1,8 +1,5 @@
-[ -d "./UX320/Mar11-angular-plugin" ] && exit
+[ -d "./UX320/FinalPractice" ] && exit
 mkdir -p UX320
 cd ./UX320
-git clone https://github.com/rhildred/wordpress-heroku.git Mar11-angular-plugin
-cd Mar11-angular-plugin/wp-content/plugins
-git clone https://github.com/rhildred/wordpress_database_admin.git
-git clone https://github.com/rhildred/carserviceform.git
-git clone https://github.com/rhildred/piday-plugin.git
+git clone https://github.com/rhildred/wordpress-heroku.git FinalPractice
+
